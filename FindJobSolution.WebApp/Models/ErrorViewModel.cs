50e56276817be068dbfec575f493c1f0c254712f
xuanthanh2609 <thanh26092000@gmail.com>
2022-09-30 00:46:08 +0700

    Add project files.

---
+++ FindJobSolution.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FindJobSolution.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
