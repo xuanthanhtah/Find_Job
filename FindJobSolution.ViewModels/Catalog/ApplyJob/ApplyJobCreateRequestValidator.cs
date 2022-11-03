1753e90d805240752a3d5830e597cfa58c5d3ad1
nhattin57 <nhattin57@gmail.com>
2022-11-03 15:13:54 +0700

    VALIDATOR SJ, AJ, RI

---
+++ FindJobSolution.ViewModels/Catalog/ApplyJob/ApplyJobCreateRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindJobSolution.ViewModels.Catalog.ApplyJob
+{
+    public class ApplyJobCreateRequestValidator : AbstractValidator<ApplyJobCreateRequest>
+    {
+        public ApplyJobCreateRequestValidator()
+        {
+            RuleFor(x => x.JobInformationId).NotEmpty().WithMessage("JobInformationId is required");
+            RuleFor(x => x.JobSeekerId).NotEmpty().WithMessage("JobSeekerId is required");
+            RuleFor(x => x.TimeApply).NotEmpty().WithMessage("TimeApply is required");
+            RuleFor(x => x.Status).NotEmpty().WithMessage("Status is required");
+        }
+    }
+}
---
+++ FindJobSolution.ViewModels/Catalog/SaveJob/SavejobCreateRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindJobSolution.ViewModels.Catalog.SaveJob
+{
+    public class SavejobCreateRequestValidator : AbstractValidator<SaveJobCreateRequest>
+    {
+        public SavejobCreateRequestValidator()
+        {
+            RuleFor(x => x.JobInformationId).NotEmpty().WithMessage("JobInformationId is required");
+            RuleFor(x => x.JobSeekerId).NotEmpty().WithMessage("JobSeekerId is required");
+            RuleFor(x => x.TimeSave).NotEmpty().WithMessage("TimeSave is required");
+            RuleFor(x => x.Status).NotEmpty().WithMessage("Status is required");
+        }
+    }
+}
