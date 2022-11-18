7059804a466cbccbbb62f24c63a0b6b6ebd5f1d0
xuanthanh2609 <thanh26092000@gmail.com>
2022-11-19 00:18:07 +0700

    fix bug recuiter

--- FindJobSolution.ViewModels/Catalog/Recruiters/RecruiterUpdateRequest.cs
+++ FindJobSolution.ViewModels/Catalog/Recruiters/RecruiterUpdateRequest.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace FindJobSolution.ViewModels.Catalog.Recruiters
 {
     public class RecruiterUpdateRequest
     {
+        [Display(Name = "Tên công Ty")]
         public string CompanyName { get; set; }
+        [Display(Name = "Địa chỉ công ty")]
         public string Address { get; set; }
+        [Display(Name = "Giới thiệu về công ty")]
         public string CompanyIntroduction { get; set; }
         public IFormFile ThumbnailRecuiter { get; set; }
+        [Display(Name = "Tên ảnh công Ty")]
         public string nameImage { get; set; }
     }
 }
