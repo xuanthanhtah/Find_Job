88e7383279f425618103802dd89dffc143f19b3e
xuanthanh2609 <thanh26092000@gmail.com>
2022-11-21 18:50:29 +0700

    up image for jobseeker

--- FindJobSolution.ViewModels/Catalog/JobSeekers/JobSeekerUpdateRequest.cs
+++ FindJobSolution.ViewModels/Catalog/JobSeekers/JobSeekerUpdateRequest.cs
@@ -2,9 +2,11 @@
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace FindJobSolution.ViewModels.Catalog.JobSeekers
 {
@@ -12,16 +14,17 @@ namespace FindJobSolution.ViewModels.Catalog.JobSeekers
     {
         //public int JobSeekerId { get; set; }
         //public int JobId { get; set; }
-        //public string Name { get; set; }
+        public string Name { get; set; }
         public string Address { get; set; }
         public string Gender { get; set; }
         public string National { get; set; }
         public decimal DesiredSalary { get; set; }
         public DateTime Dob { get; set; }
-        //public IFormFile ThumbnailCv { get; set; }
-        //public string nameCv { get; set; }
-        //public IFormFile ThumbnailAvatar { get; set; }
-        //public string nameAvatar { get; set; }
+        public IFormFile ThumbnailCv { get; set; }
+        [Display(Name = "Tên ảnh công Ty")]
+        public string nameCv { get; set; }
+        public IFormFile ThumbnailAvatar { get; set; }
+        public string nameAvatar { get; set; }
 
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
